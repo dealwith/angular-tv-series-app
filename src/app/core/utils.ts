a12eb9dbf4b6e34a773f53056cf2c1883c821eae
Gleb <krishin@inbox.ru>
2020-04-20 04:57:12 +0300

    Error in selects

--- src/app/core/utils.ts
+++ src/app/core/utils.ts
@@ -3,3 +3,10 @@ export const compare = (
   b: number | string,
   isAsc: boolean
 ) => (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+
+export const uniqueBy = (key: string, array: []) => {
+  const setStructure = new Set();
+  return array.filter(
+    (object) => !setStructure.has(object[key]) && setStructure.add(object[key])
+  );
+};
--- src/app/tv-table/tv-table.component.ts
+++ src/app/tv-table/tv-table.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
+import * as moment from 'moment';
 
 import { TV_SERIES } from './data';
-import { TVSeriesModel } from '../_interface/tv-series.model'
+import { TVSeriesModel } from '../_interface/tv-series.model';
+import { uniqueBy } from '../core/utils';
 
 @Component({
   selector: 'app-tv-table',
@@ -17,24 +19,46 @@ export class TvTableComponent implements OnInit, AfterViewInit {
 
   dataSource: MatTableDataSource<TVSeriesModel>;
   displayedColumns: string[] = ['name', 'season', 'network', 'premiere'];
+  genres: string[] = [];
+  premieres: string[] = [];
 
   constructor() {
     this.dataSource = new MatTableDataSource<TVSeriesModel>(TV_SERIES);
   }
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
+    this.allGenres();
+    this.allPremieres();
   }
 
   applyFilter(filterValue: string): void {
     filterValue = filterValue.trim();
     filterValue = filterValue.toLowerCase();
     this.dataSource.filter = filterValue;
+  }
+
+  allGenres(): void {
+    const uniqueGenres = [
+      ...new Set(
+        TV_SERIES.map((object) => object.genres).reduce((acc, val) =>
+          acc.concat(val)
+        )
+      ),
+    ];
+    this.genres = uniqueGenres;
+  }
+
+  allPremieres(): void {
+    // @ts-ignore
+    //the error in TV_SERIES
+    const uniqueYears = uniqueBy('preimere', TV_SERIES).map(
+      (uniqueObjects) => uniqueObjects.premiere
+    );
+    return uniqueYears;
   }
 
   redirectToDetails = (id: string) => {};
